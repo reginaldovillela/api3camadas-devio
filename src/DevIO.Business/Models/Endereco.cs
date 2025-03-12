e00b2365dd0faf04f95ec3accd0bd2d9dd3d6fba
unknown <43396103+reginaldovillela@users.noreply.github.com>
2025-03-11 21:58:03 -0300

    feature: criando a arquitetura inicial e as entidades base

---
+++ src/DevIO.Business/Models/Endereco.cs
@@ -0,0 +1,18 @@
+﻿namespace DevIO.Business.Models;
+
+public class Endereco : Entity
+{
+    public string? Logradouro { get; set; }
+
+    public string? Numero { get; set; }
+
+    public string? Complemento { get; set; }
+
+    public string? CEP { get; set; }
+
+    public string? Bairro { get; set; }
+
+    public string? Cidade { get; set; }
+
+    public string? Estado { get; set; }
+}
---
+++ src/DevIO.Business/Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace DevIO.Business.Models;
+
+public abstract class Entity
+{
+    public Guid Id { get; private set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+}
