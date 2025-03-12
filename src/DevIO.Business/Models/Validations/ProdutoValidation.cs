1f8efd076a22b730071c9e651ce031a891d4676b
unknown <43396103+reginaldovillela@users.noreply.github.com>
2025-03-11 22:35:33 -0300

    feat(validations): adicionando as validações

---
+++ src/DevIO.Business/Models/Validations/ProdutoValidation.cs
@@ -0,0 +1,19 @@
+﻿namespace DevIO.Business.Models.Validations;
+
+public class ProdutoValidation : AbstractValidator<Produto>
+{
+    public ProdutoValidation()
+    {
+        RuleFor(p => p.Nome)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(p => p.Descricao)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+        RuleFor(p => p.Valor)
+            .GreaterThan(0).WithMessage("O campo {PropertyName} precisa ser maior que {ComparisonValue}");
+
+    }
+}
